cf3bcbb98624d180e74f2d3bf867cb947a3ed6b7
Marcos Fonseca <marco12.fonseca>
2020-08-20 23:31:24 -0300

    funcao downloadXml foi refatorada

--- core/DownloadXML.py
+++ core/DownloadXML.py
@@ -1,38 +1,32 @@
-import urllib.request
+import urllib.request as request
+
+def _enableUserAgent():
+    client = request.build_opener()
+    client.addheaders = [(
+        'User-agent',
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36'
+    )] # User Agent
+    request.install_opener(client)
 
 def downloadXML(urls, directory='.database') -> list:
     print('[RSS] Baixando xmls ...') # log
 
-    filenames = []
-    failedUrls = []
-
-    # dowload sem user-agent
-    for x in range(len(urls)):
-        try:
-            urllib.request.urlretrieve(urls[x], f'{directory}/file{x}.xml')
-            filenames.append(f'{directory}/file{x}.xml')
-        except:
-            print(f'[RSS] erro ao tentar baixar da url {urls[x]}') # log
-            print('tentando novamente com user-agent definido')
-            failedUrls.append(urls[x])
-
-    # donwload com user-agent
-    if len(failedUrls) > 0:
-        client = urllib.request.build_opener()
-        client.addheaders = [(
-            'User-agent',
-            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36'
-        )] # User Agent
-        urllib.request.install_opener(client)
-    
-        # correcao temporaria, necessita de uma implementacao melhor!
-        x += 1
-
-        for x1 in range(len(failedUrls)):
+    x = 0
+    while True:
+        failed = []
+        for url in urls:
             try:
-                urllib.request.urlretrieve(failedUrls[x1], f'{directory}/file{x+x1}.xml')
-                filenames.append(f'{directory}/file{x+x1}.xml')
+                request.urlretrieve(url, f'{directory}/file{x}.xml')
+                x += 1
             except:
-                print(f'[RSS] erro ao tentar baixar da url {urls[x1]}') # log
+                print(f'[RSS] Erro ao tentar baixar da url: {url}')
+                failed.append(url)
 
-    return filenames
+        if len(failed) > 0:
+            print(f'[RSS] Tetando baixar novamente usando user-agent')
+            _enableUserAgent()
+            urls = failed
+        else:
+            break
+
+    return [f'{directory}/file{i}.xml' for i in range(x)]
